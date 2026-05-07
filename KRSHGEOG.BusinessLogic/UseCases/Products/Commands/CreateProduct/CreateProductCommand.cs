4ce345f8268a9294cabed6b165a97c7f0fd5af46
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-06 23:55:59 -0600

    feat: implementar handler de creación de productos - Gabriel Ernesto Obando García

---
+++ KRSHGEOG.BusinessLogic/UseCases/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,6 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Products.Commands.CreateProduct;
+
+public record CreateProductCommand(SolicitudCrearProducto Request) : IRequest<long>;

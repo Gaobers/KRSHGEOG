dc12d8d9457c1612b49600874cb8c2f8c5ec86c8
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-06 23:41:14 -0600

    feat: agregar DTOs de creación, actualización y respuesta de producto - Gabriel Ernesto Obando García

---
+++ KRSHGEOG.BusinessLogic/DTOs/ProductDto.cs
@@ -0,0 +1,38 @@
+﻿namespace KRSHGEOG.BusinessLogic.DTOs;
+
+public class SolicitudCrearProducto
+{
+    public int ToolBrandId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal? SalePrice { get; set; }
+
+    public int? StockUnits { get; set; }
+}
+
+public class SolicitudActualizarProducto
+{
+    public long Id { get; set; }
+
+    public int ToolBrandId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal? SalePrice { get; set; }
+
+    public int? StockUnits { get; set; }
+}
+
+public class ProductoResponse
+{
+    public long Id { get; set; }
+
+    public int ToolBrandId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal? SalePrice { get; set; }
+
+    public int? StockUnits { get; set; }
+}

7639d6aaaf0864cbf498a49146b0834f4e66f8aa
Rosaura <Karlarosauras@gmail.com>
2026-05-07 08:51:12 -0600

    Karla Rosaura Sánchez Hernández
    feat: agregar DTO SolicitudCrearMarca
    
    Descripción:
    
    Se crea la clase SolicitudCrearMarca dentro del namespace KRSHGEOG.BusinessLogic.DTOs.
    
    Se definen las propiedades Id y BrandName para la transferencia de datos en la creación de marcas.

---
+++ KRSHGEOG.BusinessLogic/DTOs/BrandDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRSHGEOG.BusinessLogic.DTOs
+{
+    public class SolicitudCrearMarca
+
+    {
+
+        public int Id { get; set; }
+
+        public string BrandName { get; set; } = null!;
+    }
+}

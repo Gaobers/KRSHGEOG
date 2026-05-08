3e9851ee7f755b4c686d4195f71adfd581c98884
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-07 23:44:14 -0600

    feat(products): agregar consulta para listar productos con marca - Gabriel Ernesto Obando García
    - Agrega GetProductsQuery para obtener productos
    - Implementa GetProductsHandler usando IEfRepository<HardwareProduct>
    - Incluye la relación ToolBrand mediante GetProductWithBrandSpec
    - Mapea los productos a ProductoResponse con Mapster

---
+++ KRSHGEOG.BusinessLogic/UseCases/Products/Queries/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using KRSHGEOG.BusinessLogic.UseCases.Products.Specifications;
+using KRSHGEOG.DataAccess.Interfaces;
+using KRSHGEOG.Entities;
+using Mapster;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Products.Queries.GetProducts
+{
+    internal sealed class GetProductsHandler(IEfRepository<HardwareProduct> _repository) : IRequestHandler<GetProductsQuery, List<ProductoResponse>>
+    {
+        public async Task<List<ProductoResponse>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var productos = await _repository.ListAsync(new GetProductWithBrandSpec(), cancellationToken);
+
+            if (productos == null && !productos.Any())
+            {
+                return new List<ProductoResponse>();
+            }
+
+            return productos.Adapt<List<ProductoResponse>>();
+        }
+    }
+}

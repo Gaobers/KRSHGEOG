f6984db64a7211d25ae5b685214dc22aafca1729
Rosaura <Karlarosauras@gmail.com>
2026-05-11 11:47:24 -0600

    Karla Rosaura Sánchez  Hernández feat: implementarción del manejador de comando UpdateBrand
    - Se agrega el record UpdateBrandCommand para las solicitudes de actualización.
    - Se implementa UpdateBrandHandler para gestionar la lógica de actualización.
    - Se integra la búsqueda previa por ID y el mapeo de cambios con Mapster.
    - Se actualiza la persistencia a través del repositorio.

---
+++ KRSHGEOG.BusinessLogic/UseCases/Brands/Commands/UpdateBrand/UpdateBrandHandler.cs
@@ -0,0 +1,34 @@
+﻿using KRSHGEOG.BusinessLogic.UseCases.Brands.Commands.CreateBrands;
+using KRSHGEOG.DataAccess.Interfaces;
+using Mapster;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Brands.Commands.UpdateBrand;
+
+internal sealed class UpdateBrandHandler(IEfRepository<Brand> _repository)
+    : IRequestHandler<UpdateBrandCommand, int>
+{
+    public async Task<int> Handle(UpdateBrandCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var existingBrand = await _repository.GetByIdAsync(command.Request.Id);
+
+            if (existingBrand is null) return 0;
+
+            existingBrand = command.Request.Adapt(existingBrand);
+
+            await _repository.UpdateAsync(existingBrand, cancellationToken);
+
+            return existingBrand.Id;
+
+        }
+        catch (Exception)
+        {
+
+
+            throw;
+
+        }
+    }
+}

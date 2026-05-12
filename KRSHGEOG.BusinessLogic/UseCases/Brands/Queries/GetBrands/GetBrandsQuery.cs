4bff088e5788cc3dfbb26ccb6c3ea7ff42e2240b
Rosaura <Karlarosauras@gmail.com>
2026-05-12 10:39:56 -0600

    Karla Rosaura Sánchez Hernández feat: implementar la consulta GetBrands para listar marcas
    - Se define el record GetBrandsQuery para la solicitud de listado.
    - Se implementa GetBrandsHandler para obtener todas las marcas del repositorio.
    - Se utiliza Mapster para proyectar las entidades a BrandResponse.
    - Se añade validación para retornar una lista vacía en caso de no haber registros.

---
+++ KRSHGEOG.BusinessLogic/UseCases/Brands/Queries/GetBrands/GetBrandsQuery.cs
@@ -0,0 +1,9 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Brands.Queries.GetBrands;
+
+public record GetBrandsQuery() : IRequest< List <BrandResponse>>;

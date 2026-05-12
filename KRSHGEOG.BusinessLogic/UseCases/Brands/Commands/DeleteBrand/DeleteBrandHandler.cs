d4d814eddeb787a347c844562533a496043177b7
Rosaura <Karlarosauras@gmail.com>
2026-05-12 08:16:02 -0600

    Karla Rosaura Sánchez Hernández
    feat : implementar el manejador de comando DeleteBrand
    
    - Se define DeleteBrandCommand para la eliminación por ID.
    - Se implementa DeleteBrandHandler con validación de existencia previa.
    - Se integra la eliminación lógica o física a través del repositorio.
    - Se retorna el ID de la marca eliminada como confirmación.

---
+++ KRSHGEOG.BusinessLogic/UseCases/Brands/Commands/DeleteBrand/DeleteBrandHandler.cs
@@ -0,0 +1,24 @@
+﻿using KRSHGEOG.BusinessLogic.UseCases.Brands.Commands.CreateBrands;
+using KRSHGEOG.DataAccess.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Brands.Commands.DeleteBrand;
+
+internal sealed class DeleteBrandHandler(IEfRepository<Brand> _repository)
+    : IRequestHandler<DeleteBrandCommand, int>
+{
+    public async Task<int> Handle(DeleteBrandCommand command, CancellationToken cancellationToken)
+    {
+        var existingBrand = await _repository.GetByIdAsync(command.Id);
+
+        if (existingBrand is null) return 0;
+
+        await _repository.DeleteAsync(existingBrand, cancellationToken);
+
+        return existingBrand.Id;
+
+    }
+}

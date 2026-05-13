8740ee1914aced15af59609d2038c294de69b84b
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-13 01:36:33 -0600

    feat(users): agregar consulta para obtener usuario por ID - Gabriel Ernesto Obando García
    - Agrega GetUserQuery para consultar un usuario por ID
    - Implementa GetUserHandler usando IEfRepository<User>
    - Retorna UserByIdResponse mediante Mapster
    - Maneja el caso cuando el usuario no existe

---
+++ KRSHGEOG.BusinessLogic/UseCases/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,6 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Users.Queries.GetUser;
+
+public record GetUserQuery(int Id) : IRequest<UserByIdResponse>;

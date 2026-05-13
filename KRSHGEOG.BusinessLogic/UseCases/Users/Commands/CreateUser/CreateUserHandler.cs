1f795b7a2503c52988d2bfd8ebe7a90b18c95966
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-13 01:34:25 -0600

    feat(users): agregar comandos para crear y actualizar usuarios - Gabriel Ernesto Obando García
    - Agrega CreateUserCommand y CreateUserHandler
    - Agrega UpdateUserCommand y UpdateUserHandler
    - Implementa creación de usuarios usando IEfRepository<User>
    - Implementa actualización de usuarios existentes por ID
    - Usa Mapster para mapear los DTOs hacia la entidad User
    - Retorna el ID del usuario creado o actualizado

---
+++ KRSHGEOG.BusinessLogic/UseCases/Users/Commands/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using KRSHGEOG.DataAccess.Interfaces;
+using KRSHGEOG.Entities;
+using Mapster;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Users.Commands.CreateUser;
+
+internal sealed class CreateUserHandler(IEfRepository<User> _repository)
+    : IRequestHandler<CreateUserCommand, int>
+{
+    public async Task<int> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var nuevoUsuario = command.Request.Adapt<User>();
+            var crearUsuario = await _repository.AddAsync(nuevoUsuario, cancellationToken);
+            return crearUsuario.Id;
+        }
+        catch (Exception)
+        {
+            return 0;
+            throw;
+        }
+
+    }
+}
---
+++ KRSHGEOG.BusinessLogic/UseCases/Users/Commands/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,33 @@
+﻿using KRSHGEOG.DataAccess.Interfaces;
+using KRSHGEOG.Entities;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Users.Commands.UpdateUser;
+
+internal sealed class UpdateUserHandler(IEfRepository<User> _repository)
+    : IRequestHandler<UpdateUserCommand, int>
+{
+    public async Task<int> Handle(UpdateUserCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var usuarioExistente = await _repository.GetByIdAsync(command.Request.Id, cancellationToken);
+
+            if (usuarioExistente is null) return 0;
+
+            usuarioExistente = command.Request.Adapt(usuarioExistente);
+            await _repository.UpdateAsync(usuarioExistente, cancellationToken);
+
+            return usuarioExistente.Id;
+        }
+        catch (Exception)
+        {
+            return 0;
+            throw;
+        }
+    }
+}

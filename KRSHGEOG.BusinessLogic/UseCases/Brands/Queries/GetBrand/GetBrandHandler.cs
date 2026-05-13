f3a4eb973d4ed9651db0729784d7c62261ef74da
Rosaura <Karlarosauras@gmail.com>
2026-05-12 19:55:19 -0600

    Karla Rosaura Sánchez Hernández feat: implementar la consulta GetBrand por ID
    - Se define el record GetBrandQuery para buscar una marca específica.
    - Se implementa GetBrandHandler para obtener la entidad desde el repositorio.
    - Se integra el mapeo de la entidad a BrandResponse usando Mapster.
    - Se añade manejo de nulos retornando una respuesta vacía.

---
+++ KRSHGEOG.BusinessLogic/UseCases/Brands/Queries/GetBrand/GetBrandHandler.cs
@@ -0,0 +1,25 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using KRSHGEOG.BusinessLogic.UseCases.Brands.Commands.CreateBrands;
+using KRSHGEOG.DataAccess.Interfaces;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Brands.Queries.GetBrand;
+
+internal sealed class GetBrandHandler(IEfRepository<Brand> _repository)
+    : IRequestHandler<GetBrandQuery, BrandResponse>
+{
+    public async Task<BrandResponse> Handle(GetBrandQuery query, CancellationToken cancellationToken)
+    {
+        var brand = await _repository.GetByIdAsync(query.Id, cancellationToken);
+
+        if (brand is null) {
+            return new BrandResponse();
+        }
+        return brand.Adapt<BrandResponse>();
+    }
+}
+
---
+++ KRSHGEOG.BusinessLogic/UseCases/Brands/Queries/GetBrand/GetBrandQuery.cs
@@ -0,0 +1,7 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Brands.Queries.GetBrand;
+
+public record GetBrandQuery(int Id) : IRequest<BrandResponse>;
+

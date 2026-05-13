25df2aa0b71048bcbd59cdaa1bbfb90f7bfeadef
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-12 23:52:30 -0600

    feat(mappings): registrar mapeo de HardwareProduct a ProductoResponse - Gabriel Ernesto Obando García

---
+++ KRSHGEOG.BusinessLogic/Mappings/MappingRegister.cs
@@ -0,0 +1,14 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using KRSHGEOG.Entities;
+using Mapster;
+
+namespace KRSHGEOG.BusinessLogic.Mappings
+{
+    public class MappingRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<HardwareProduct, ProductoResponse>();
+        }
+    }
+}

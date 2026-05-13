8b3fa93fb0ec7ae8cbfd95e22470bf6616b0a801
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-13 01:37:37 -0600

    feat(users): agregar consulta para listar usuarios - Gabriel Ernesto Obando García
    - Agrega GetUsersQuery para consultar la lista de usuarios
    - Implementa GetUsersHandler usando IEfRepository<User>
    - Retorna una lista vacía cuando no existen usuarios
    - Mapea las entidades User a UserResponse con Mapster

---
+++ KRSHGEOG.BusinessLogic/UseCases/Users/Queries/GetUsers/GetUsersHandler.cs
@@ -0,0 +1,23 @@
+﻿using KRSHGEOG.BusinessLogic.DTOs;
+using KRSHGEOG.DataAccess.Interfaces;
+using KRSHGEOG.Entities;
+using Mapster;
+using MediatR;
+
+namespace KRSHGEOG.BusinessLogic.UseCases.Users.Queries.GetUsers;
+
+internal sealed class GetUsersHandler(IEfRepository<User> _repository)
+    : IRequestHandler<GetUsersQuery, List<UserResponse>>
+{
+    public async Task<List<UserResponse>> Handle(GetUsersQuery query, CancellationToken cancellationToken)
+    {
+        var usuarios = await _repository.ListAsync(cancellationToken);
+
+        if (usuarios == null || !usuarios.Any())
+        {
+            return new List<UserResponse>();
+        }
+
+        return usuarios.Adapt<List<UserResponse>>();
+    }
+}
